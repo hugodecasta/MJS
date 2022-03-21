50ac3453a4c97208f6863be486c959791dca9c66
hugodecasta <hugo.castaneda.dev@gmail.com>
2022-03-21 23:05:47 +0100

    reset files

--- swagger.js
+++ swagger.js
@@ -10,6 +10,7 @@ const doc = {
         title: 'Majority Judgment Server',
         description: 'MJS API documentation',
     },
+    host: process.env.HOST ?? ('localhost:' + process.env.PORT ?? 3000),
     basePath: '/api',
     schemes: ['http', 'https'],
     securityDefinitions: {
